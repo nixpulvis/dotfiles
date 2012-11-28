6467c83b0eea7a6b5e8a8a4a4fba730c6875fadd
Nathan Lilienthal <nathanlil13@me.com>
2012-11-27 23:35:45 -0500

    fucking colors ls my god

--- aliases/directory.zsh
+++ aliases/directory.zsh
@@ -1,3 +1,12 @@
+# Detect which `ls` flavor is in use
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+  colorflag="--color"
+else # OS X `ls`
+  colorflag="-G"
+fi
+
+alias ls="ls ${colorflag}"
+
 # List all files colorized in long format
 alias l="ls -l ${colorflag}"
 
