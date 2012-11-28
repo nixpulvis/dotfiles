35fe6a780d71978bafcc6305819b4519fc3cc8a8
Nathan Lilienthal <nathanlil13@me.com>
2012-11-27 22:38:44 -0500

    usefull aliases

---
+++ aliases/directory.zsh
@@ -0,0 +1,8 @@
+# List all files colorized in long format
+alias l="ls -l ${colorflag}"
+
+# List all files colorized in long format, including dot files
+alias la="ls -la ${colorflag}"
+
+# List only directories
+alias lsd='ls -l ${colorflag} | grep "^d"'
---
+++ aliases/navigation.zsh
@@ -0,0 +1,10 @@
+# Get back
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+# Places I like to travel
+alias dl="cd ~/Downloads"
+alias dt="cd ~/Desktop"
+alias gh="cd ~/Documents/github"
