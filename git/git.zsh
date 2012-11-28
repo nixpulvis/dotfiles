a1baa8bbe178b64dfd38de215e4dc4820ea9ff2f
Nathan Lilienthal <nathanlil13@me.com>
2012-11-27 22:27:04 -0500

    sexy git info

---
+++ git/git.zsh
@@ -0,0 +1,12 @@
+autoload -Uz vcs_info
+
+# compute git status before every command.
+precmd() { vcs_info }
+
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:git*:*' get-revision true
+zstyle ':vcs_info:git*:*' check-for-changes true
+
+zstyle ':vcs_info:git*' formats "on ${PURPLE}%b%u%c${RESET}"
+zstyle ':vcs_info:git*' stagedstr "^"
+zstyle ':vcs_info:git*' unstagedstr "*"
