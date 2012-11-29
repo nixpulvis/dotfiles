ab3bb1c34b5bf0f611299ac4ce10704ee2ffc87a
Nathan Lilienthal <nathanlil13@me.com>
2012-11-29 16:16:03 -0500

    simplify battery

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,6 +1,6 @@
-function battery_charge {
-    echo `battery`
-}
+# function battery_charge {
+#     echo `battery`
+# }
 
 # Allow expansion inside PROMPT
 setopt prompt_subst
@@ -9,4 +9,4 @@ setopt prompt_subst
 PROMPT='${MAGENTA}%n${RESET} at ${ORANGE}%m${RESET} in ${GREEN}%c${RESET} ${vcs_info_msg_0_}
 $ '
 
-RPROMPT='%10{$(battery)%}'
+RPROMPT='%10{$(echo `battery`)%}'
