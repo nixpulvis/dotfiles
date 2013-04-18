eef5ad92dad11e1bf5d363f4f4d013a593cf03a8
Nathan Lilienthal <nathan@nixpulvis.com>
2013-04-18 15:11:24 -0400

    installing will setup merge/rewrite hooks

--- install.sh
+++ install.sh
@@ -3,19 +3,25 @@
 
 echo "Installing .:. dotfiles"
 
-dotfiles=$HOME/.dotfiles
+DOTFILES=$HOME/.dotfiles
 
 # Move dotfiles to the proper dotfiles path if they are not there already.
 #TODO
 
+# Install git post hooks.
+hooks=$DOTFILES/.git/hooks
+echo "#!/bin/sh
+exec \$DOTFILES/install.sh" | tee $hooks/post-merge > $hooks/post-rewrite
+chmod +x $hooks/post-merge $hooks/post-rewrite
+
 # Ensure that we are work within the acceptable working directory.
-if [[ $PWD != $dotfiles ]]; then
-  echo "Must be inside $dotfiles"
+if [[ $PWD != $DOTFILES ]]; then
+  echo "Must be inside $DOTFILES"
   exit 1
 fi
 
 # Symlink ".link" files.
-for file ($dotfiles/**/*.link) do
+for file ($DOTFILES/**/*.link) do
   line=$(head -n 1 $file)
   eval destination=${line//(\#|\")=>/}
   ln -fs $file $destination
