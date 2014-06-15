c14cee47a95cf21549e9567cdb5c6266ad081777
Nathan Lilienthal <nathan@nixpulvis.com>
2014-06-15 17:43:06 -0400

    Make the install script not depend on zsh.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/sh
 # Don't be a fool.
 
 echo -n ".:. Installing dotfiles... "
@@ -6,18 +6,17 @@ echo -n ".:. Installing dotfiles... "
 DOTFILES=$HOME/.dotfiles
 
 # Ensure that we are work within the acceptable working directory.
-if [[ $PWD != $DOTFILES ]]; then
+if [ $PWD != $DOTFILES ]; then
   echo "Must be inside $DOTFILES"
   exit 1
 fi
 
 # Symlink ".link" files.
-for file ($DOTFILES/**/*.link) do
-  line=$(head -n 1 $file)
-  eval destination=${line//(\#|\"|\/\/)=>/}
+for file in $DOTFILES/**/*.link; do
+  destination=$(head -n 1 $file | sed "s/[#|\"|;|//]*=>//")
+  echo "Linking $file to $destination"
   mkdir -p `dirname $destination`
   ln -fs $file $destination
 done
 
-echo "done"
 echo ".:. Please run \"exec fish\" in current shells."
