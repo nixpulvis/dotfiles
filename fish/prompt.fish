1ccf63116f055228bce73f61fc5526492b35d46a
Nathan Lilienthal <nathan@nixpulvis.com>
2015-12-21 20:57:36 -0500

    New prompt for a new year

--- fish/prompt.fish
+++ fish/prompt.fish
@@ -1,55 +1,91 @@
-function is_git_repo
-  git rev-parse >/dev/null ^/dev/null
-  not test $status -eq 128
-end
-
-function section
-  printf "%s$argv[1]:" (set_color cyan)
-  printf "%s$argv[2] " (set_color green)
-end
-
-function error
-  printf "%s$argv[1]:" (set_color cyan)
-  printf "%s$argv[2] " (set_color red)
-end
-
 function fish_prompt --description 'Write out the prompt'
-    # $status gets nuked as soon as something else is run, e.g. set_color
-    # so it has to be saved asap.
-    set -l last_status $status
+	set -l last_status $status
 
-    # Username
-    printf "%s%s " (set_color red) (whoami)
+	# Just calculate this once, to save a few cycles when displaying the prompt
+	if not set -q __fish_prompt_hostname
+		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+	end
 
-    # Hostname
-    printf "%son %s%s " (set_color white) (set_color magenta) (hostname)
+	set -l normal (set_color normal)
 
-    # PWD.
-    printf "%sin %s%s " (set_color white) (set_color green) (basename $PWD)
+	# Hack; fish_config only copies the fish_prompt function (see #736)
+	if not set -q -g __fish_classic_git_functions_defined
+		set -g __fish_classic_git_functions_defined
 
-    printf "%swith " (set_color white)
+		function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
+			if status --is-interactive
+				commandline -f repaint ^/dev/null
+			end
+		end
 
-    # Last status.
-    if [ $last_status -ne 0 ]
-        error status $last_status
-        set -ge status
-    end
+		function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
+			if status --is-interactive
+				commandline -f repaint ^/dev/null
+			end
+		end
 
-    # Show disk usage when low
-    set -l du (df / | tail -n1 | sed "s/  */ /g" | cut -d' ' -f 5 | cut -d'%' -f1)
-    if test $du -gt 80
-        error du $du%%
-    end
+		function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
+			if status --is-interactive
+				commandline -f repaint ^/dev/null
+			end
+		end
 
-    # rbenv
-    if type ruby > /dev/null 2>&1
-      section ruby (ruby --version | cut -d' ' -f 2)
-    end
+		function __fish_repaint_bind_mode --on-variable fish_key_bindings --description "Event handler; repaint when fish_key_bindings changes"
+			if status --is-interactive
+				commandline -f repaint ^/dev/null
+			end
+		end
 
-    if is_git_repo
-      section git (__fish_git_prompt | sed -e 's/^ (*//' -e 's/) *$//')
-    end
+		# initialize our new variables
+		if not set -q __fish_classic_git_prompt_initialized
+			set -qU fish_color_user; or set -U fish_color_user -o green
+			set -qU fish_color_host; or set -U fish_color_host -o cyan
+			set -qU fish_color_status; or set -U fish_color_status red
+			set -U __fish_classic_git_prompt_initialized
+		end
+	end
 
-    # Prompt on a new line
-    printf "\n%s> " (set_color white)
+	set -l color_cwd
+	set -l prefix
+	switch $USER
+	case root toor
+		if set -q fish_color_cwd_root
+			set color_cwd $fish_color_cwd_root
+		else
+			set color_cwd $fish_color_cwd
+		end
+		set suffix '#'
+	case '*'
+		set color_cwd $fish_color_cwd
+		set suffix '>'
+	end
+
+	set -l prompt_status
+	if test $last_status -ne 0
+		set prompt_status ' ' (set_color $fish_color_status) "[$last_status]" "$normal"
+	end
+
+	set -l mode_str
+	switch "$fish_key_bindings"
+	case '*_vi_*' '*_vi'
+		# possibly fish_vi_key_bindings, or custom key bindings
+		# that includes the name "vi"
+		set mode_str (
+			echo -n " "
+			switch $fish_bind_mode
+			case default
+				set_color --bold --background red white
+				echo -n "[N]"
+			case insert
+				set_color --bold green
+				echo -n "[I]"
+			case visual
+				set_color --bold magenta
+				echo -n "[V]"
+			end
+			set_color normal
+		)
+	end
+
+	echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" "> "
 end
