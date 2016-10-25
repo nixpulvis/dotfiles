947200a16f9c4ec2f552d3eee85397814820588f
Nathan Lilienthal <nlilienthal@apple.com>
2016-10-24 18:54:09 -0700

    Adding a footer to each command.

--- fish/prompt.fish
+++ fish/prompt.fish
@@ -1,3 +1,7 @@
+function foo
+	echo (seq (tput cols))
+end
+
 function fish_prompt --description 'Write out the prompt'
 	set -l last_status $status
 
@@ -87,5 +91,13 @@ function fish_prompt --description 'Write out the prompt'
 		)
 	end
 
+	set_color --background brgrey
+	set_color black
+	set W_COLS (tput cols)
+	for x in (seq (math $W_COLS - 30))
+    echo -n '-'
+	end
+	echo [(date)]
+	set_color normal
 	echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" "> "
 end
