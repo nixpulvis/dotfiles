a253233247865d5109ad6f3ed49639da1e8857ea
Nathan Lilienthal <nathan@nixpulvis.com>
2016-11-05 16:21:24 -0700

    This function obviously belongs here.

--- fish/.config/fish/install/lib.fish
+++ fish/.config/fish/install/lib.fish
@@ -1,2 +1,9 @@
+function dotfiles.install -d "TODO"
+  echo "TRACE: dotfiles.install $argv"
+  emit preinstall:package:$argv
+  stow $argv
+  emit postinstall:package:$argv
+end
+
 source $LIB/install/hooks/lib.fish
 source $LIB/install/system.fish
