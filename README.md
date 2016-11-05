1e253a88d66c83069db47503e333435a126a6876
Nathan Lilienthal <nathan@nixpulvis.com>
2016-11-05 16:53:40 -0700

    Use dotfiles.install over ./main.fish.

--- README.md
+++ README.md
@@ -8,15 +8,16 @@ little fishy. Run the following command to get with the times:
 ./fishup
 ```
 
-Once you have `fish`, installing for the first, second, third or even forth time can be done with the following command:
+Once you have `fish`, installing for the first, second, third or even forth
+time can be done with the following command:
 
 ```
 # Full install.
-./install
+dotfiles.install
 
 # To skip the slow Atom package install phase.
 # Caused by https://github.com/atom/apm/issues/170.
-env SKIP_APM_STARS_INSTALL=1 ./install.fish
+env SKIP_APM_STARS_INSTALL=1 dotfiles.install
 ```
 
 Make sure you read the output of these commands, there are still a
--- fish/.config/fish/install.fish
+++ fish/.config/fish/install.fish
@@ -1,9 +1,36 @@
 # You install packages.
 function dotfiles.install -d "TODO"
   echo "TRACE: dotfiles.install $argv"
-  emit preinstall:package:$argv
-  stow $argv
-  emit postinstall:package:$argv
+
+  switch (count $argv)
+  case 0
+    # First we update the dotfiles with git. Staying up to date with the public
+    # version of the code is important to me.
+    git pull
+    git submodule init
+    git submodule update --recursive --remote
+
+    # Install the packages and run thier pre and post install hooks. The order here
+    # matters quite a lot, and can be tricky.
+    switch (uname)
+    case Darwin
+      dotfiles.install macos
+    case Linux
+      dotfiles.install linux
+    end
+    # TODO: Make a package as well, allowing install package to be simply install.
+    dotfiles.install.system
+    dotfiles.install atom
+    dotfiles.install fish
+    dotfiles.install bin
+    dotfiles.install git
+    dotfiles.install racket
+    dotfiles.install ruby
+  case 1
+    emit preinstall:package:$argv
+    stow $argv
+    emit postinstall:package:$argv
+  end
 end
 
 source $LIB/hooks/lib.fish
