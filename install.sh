7a15831f5639e12d13e740375785c6dd50a0c2b1
Nathan Lilienthal <nathan@nixpulvis.com>
2016-11-05 17:41:10 -0700

    Add a direct command for installing.

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 if ! type "fish" 2&> /dev/null; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" </dev/null
-  echo "hit1"
   brew install fish
 fi
-echo
-echo "Now run \`fish\` to get started. Within fish \`dotfiles.install\` will \
-install the complete system."
+export LIB=fish/.config/fish
+exec fish -c "source $LIB/install.fish" -c "dotfiles.install"
