b0b8594640f11a96409fc4ed8bfff0d54fe29f2d
Nathan Lilienthal <nathan@nixpulvis.com>
2017-06-02 14:51:38 -0700

    Add back some of my aliases.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,15 @@ set -x EDITOR emacs
 set -x PATH $HOME/.cargo/bin $PATH
 set -x PATH ~/.rbenv/shims $PATH
 set -x PATH ~/.pyenv/shims $PATH
+
+alias .. "cd .."
+alias ... "cd ../.."
+alias .... "cd ../../.."
+alias ..... "cd ../../../.."
+
+alias l "ls -l"
+alias la "ls -la"
+
+alias code "cd ~/Code"
+alias dl "cd ~/Downloads"
+alias dt "cd ~/Desktop"
