75c921b47e819653a27c4b07ffbce187c6ecb4ba
Nathan Lilienthal <nathan@nixpulvis.com>
2021-07-09 12:34:17 -0400

    Sway idle bb

--- sway.nix
+++ sway.nix
@@ -168,44 +168,43 @@ in {
 
   systemd.user.services.sway = {
     Unit = {
-    Description = "Sway - Wayland window manager";
-    Documentation = [ "man:sway(5)" ];
-    BindsTo = [ "graphical-session.target" ];
-    Wants = [ "graphical-session-pre.target" ];
-    After = [ "graphical-session-pre.target" ];
+      Description = "Sway - Wayland window manager";
+      Documentation = [ "man:sway(5)" ];
+      BindsTo = [ "graphical-session.target" ];
+      Wants = [ "graphical-session-pre.target" ];
+      After = [ "graphical-session-pre.target" ];
     };
     Service = {
-    Type = "simple";
-    ExecStart = "${pkgs.sway}/bin/sway";
-    Restart = "on-failure";
-    RestartSec = 1;
-    TimeoutStopSec = 10;
+      Type = "simple";
+      ExecStart = "${pkgs.sway}/bin/sway";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
     };
   };
 
-  # systemd.user.services.swayidle = {
-  #   enable = true;
-  #   description = "swayidle locking";
-  #   requiredBy = [ "graphical-session.target" ];
-  #   unitConfig = {
-  #     PartOf = [ "graphical-session.target" ];
-  #     ConditionGroup = "users";
-  #   };
-  #   path = with pkgs; [ bash strace swayidle swaylock sway ];
-  #   script = ''
-  #     swayidle -w \
-  #     timeout 150 'swaylock -elfF -s fill -i ${.X/backdrop1.png}' \
-  #     timeout 300 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' \
-  #     before-sleep 'swaylock -elfF -s fill -i ${.X/backdrop1.png}'
-  #     lock 'swaylock -elfF -s fill -i ${.X/backdrop1.png}'
-  #   '';
-  #   # script = ''
-  #   #   swayidle -w \
-  #   #   timeout 120 '${pkgs.backlight-locker} down' resume '${pkgs.backlight-locker} up' \
-  #   #   timeout 150 'swaylock -elfF -s fill -i ${../../nixos-nineish.png}' \
-  #   #   timeout 300 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' \
-  #   #   before-sleep 'swaylock -elfF -s fill -i ${../../nixos-nineish.png}'
-  #   #   lock 'swaylock -elfF -s fill -i ${../../nixos-nineish.png}'
-  #   # '';
-  # };
+    # path = with pkgs; [ bash strace swayidle swaylock sway ];
+  systemd.user.services.swayidle = {
+    Unit = {
+      Description = "swayidle locking";
+      PartOf = [ "graphical-session.target" ];
+      ConditionGroup = "users";
+    };
+    Service = {
+      Type = "simple";
+      ExecStart = ''
+        ${pkgs.swayidle}/bin/swayidle -w timeout 150 'swaylock -elfF -s fill -i ${.X/backdrop1.png}' timeout 300 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' before-sleep 'swaylock -elfF -s fill -i ${.X/backdrop1.png}' lock 'swaylock -elfF -s fill -i ${.X/backdrop1.png}'
+      '';
+    };
+
+    # TODO: What is this?
+    # script = ''
+    #   swayidle -w \
+    #   timeout 120 '${pkgs.backlight-locker} down' resume '${pkgs.backlight-locker} up' \
+    #   timeout 150 'swaylock -elfF -s fill -i ${../../nixos-nineish.png}' \
+    #   timeout 300 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' \
+    #   before-sleep 'swaylock -elfF -s fill -i ${../../nixos-nineish.png}'
+    #   lock 'swaylock -elfF -s fill -i ${../../nixos-nineish.png}'
+    # '';
+  };
 }
